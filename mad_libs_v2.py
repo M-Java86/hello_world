d30eca2705d095b4381ada4430a9d0f5d808ca5e
M-Java86 <50181101+M-Java86@users.noreply.github.com>
2019-09-08 23:45:50 -0400

    Week 1/Day 1

---
+++ mad_libs_v2.py
@@ -0,0 +1,7 @@
+person = input("what is your name?")
+subject = input("what is your favorite subject?")
+
+def fix_name(person):
+     return person
+     
+madlib = print()     
---
+++ prompt.py
@@ -0,0 +1,4 @@
+user_input =int(input("Enter a number"))
+
+result =user_input * user_input
+print(result) 
